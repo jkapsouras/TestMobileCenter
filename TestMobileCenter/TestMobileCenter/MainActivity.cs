8dc8ca1e8859a9b49d6ab3e20bc60bb84b3509b8
Ioannis Kapsouras <jkapsouras@san-soft>
2017-01-22 11:43:02 +0200

    test

---
+++ TestMobileCenter/TestMobileCenter/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System;
+
+namespace TestMobileCenter
+{
+    [Activity(Label = "TestMobileCenter", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            int y = 1;
+            int x = 5;
+            MobileCenter.Start("5cd5e970-8a56-4c96-87f0-48a48c4483bd",
+                    typeof(Analytics), typeof(Crashes));
+            Console.WriteLine(x / (y - 1));
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
